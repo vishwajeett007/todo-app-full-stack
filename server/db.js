65492d236a2e1261b5fb3176884c16470c27a214
vishwajeett007 <vishwajeetrajput282@gmail.com>
2026-07-13 01:06:29 +0530

    fix error

--- server/db.js
+++ server/db.js
@@ -5,7 +5,8 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 })
 
 module.exports = pool;
